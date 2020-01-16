0d10fb5c71f6c6743aead4a7315b35c7527637a8
Luciano Adonis <luciano.adonis@dxc.com>
2020-01-16 16:14:38 -0300

    Fix

--- pathfinder.rb
+++ pathfinder.rb
@@ -1,10 +1,7 @@
-# frozen_string_literal: true
-
 require 'twitter'
 require './config/yaml_config'
 require './social/web_slack'
 
-# Just for run
 class Finder
   include WebSlack
   include Credentials
@@ -16,11 +13,8 @@ class Finder
     @accounts.each do |account, v|
       threads << Thread.new do
         client = Twitter::Streaming::Client.new(@twitter_tokens)
-        user_id = v[0]
-        user_name = v[1]
-        content_filter = v[2]
-        p "Reading tuits from #{account}: #{user_name}"
-        client.filter(follow: user_id) do |tweet|
+        p "Reading tuits from #{account}"
+        client.filter(follow: v[0]) do |tweet|
           if tweet.is_a?(Twitter::Tweet) && !tweet.text.include?((v[2]).to_s)
             send_to_channel(@slack_token, tweet.uri)
           end
