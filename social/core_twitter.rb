ddf9aac6be487ae25fb998d652ec89607cea448a
Luciano Adonis <luciano.adonis@dxc.com>
2020-03-06 23:47:50 -0300

    Improvement over output options on twitter class

--- social/core_twitter.rb
+++ social/core_twitter.rb
@@ -4,13 +4,23 @@ require './social/core_slack'
 
 # Class for all interactions related to twitter
 class CoreTwitter < Credentials
-  def initialize(token = nil)
+  def initialize(output = 'slack', token = nil)
+    @social_media = output
     @client ||= Twitter::Streaming::Client.new(token.nil? ? twitter_token : token)
   end
 
-  def share_url(url)
+  def to_slack
     slack = CoreSlack.new
-    slack.send_to_channel(url)
+    slack.send_channel_message(@tweet.url)
+  end
+
+  def share_tweet
+    case @social_media
+    when 'slack'
+      to_slack
+    else
+      print "#{@tweet.text}\n"
+    end
   end
 
   def streaming(account, pattern = '(.*)', filter = '^(RT @|@)')
@@ -18,7 +28,8 @@ class CoreTwitter < Credentials
     @client.filter(follow: account) do |tweet|
       text = tweet.text
       if tweet.user.id == account.to_i && text.match(pattern) && text.match(filter).nil?
-        share_url(tweet.url)
+        @tweet = tweet
+        share_tweet
       end
     end
   rescue StandardError
