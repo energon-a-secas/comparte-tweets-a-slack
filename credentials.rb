4cd6fe1f982238490bbc683994563f69ca4deec6
Luciano Adonis <luciano.adonis@dxc.com>
2020-03-08 21:43:04 -0300

    Change YAML definitions and add exception for missing conf.yml file

--- credentials.rb
+++ credentials.rb
@@ -1,27 +1,29 @@
 require 'yaml'
 
 class Credentials
-
   def load(file = 'conf.yml')
     YAML.load_file("#{Dir.pwd}/config/#{file}")
+  rescue Errno::ENOENT => e
+    p e.message
+    exit
   end
 
   def slack_token
     credentials = load
-    credentials['slack']['token']['test']
+    credentials['slack']['tokens']['prod']
   end
 
   def twitter_token
     credentials = load
-    twitter = credentials['twitter']['token']
+    twitter = credentials['twitter']['config']['tokens']
     { consumer_key: twitter['consumer_key'],
       consumer_secret: twitter['consumer_secret'],
       access_token: twitter['access_token'],
       access_token_secret: twitter['access_token_secret'] }
   end
 
-  def twitter_account
+  def twitter_follow
     credentials = load
-    credentials['twitter']['config']['follow']
+    credentials['twitter']['config']['accounts']
   end
 end
