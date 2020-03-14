8b632b601aa6a20031b03c13f1d55992be2f0c5f
Luciano Adonis <luciano.adonis@dxc.com>
2020-03-14 01:33:19 -0300

    Lint fix

--- spec/unit/_core_slack_spec.rb
+++ spec/unit/_core_slack_spec.rb
@@ -1,28 +1,33 @@
 require_relative '../spec_helper'
 
 describe CoreSlack do
-  before(:all) do
-    @slack = described_class.new
-  end
-
   describe '#send_to_channel' do
+    let(:text) { 'RTest' }
+    let(:test_channel) { 'hq-alerts' }
+    let(:fake_channel) { 'fake channel' }
+    let(:slack) { described_class.new }
+
     context 'when post succeeds' do
-      it { expect(@slack.send_channel_message('RTest', 'hq-enerlogs').ok).to be true }
-      it { expect(@slack.send_channel_message('RTest', 'hq-enerlogs').message.text).to eql 'RTest' }
+      it { expect(slack.send_channel_message(text, test_channel).ok).to be true }
+      it { expect(slack.send_channel_message(text, test_channel).message.text).to eql text }
     end
 
     context 'when post fails' do
-      it { expect(@slack.send_channel_message('RTest', 'fake channel')).to be false }
+      it { expect(slack.send_channel_message(text, fake_channel)).to be false }
     end
   end
 
   describe '#direct_message_id' do
+    let(:test_id) { 'j' }
+    let(:fake_id) { 'FAKEID123' }
+    let(:slack) { described_class.new }
+
     context 'when get id succeeds' do
-      it { expect(@slack.direct_message_id('j')).equal?(Hash) }
+      it { expect(slack.direct_message_id(test_id)).equal?(Hash) }
     end
 
     context 'when get id fails' do
-      it { expect(@slack.direct_message_id('FAKEID123')).to be false }
+      it { expect(slack.direct_message_id(fake_id)).to be false }
     end
   end
 end
